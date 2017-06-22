ba6b98f20a0bb8b5304a3c407fa05326b5fcec4d
Lori Landay <llanday@berklee.edu>
2017-06-22 10:39:48 -0400

    Add Textures/Scripts Folder. Add texture to fish.

---
+++ Assets/Scripts/FishMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour
+{
+    private Vector3 pos;
+    [Range(-5,5)]
+    public float speed = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        // starting
+        pos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mathf.Sin
+        pos += Vector3.back * Time.deltaTime * speed;
+        gameObject.transform.position = pos;
+   
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+    }
+}
