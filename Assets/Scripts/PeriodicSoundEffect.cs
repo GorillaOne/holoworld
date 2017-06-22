d0da8dd962ab58f8dbac12a927f923ff560b59c0
Jesse Crafts-Finch <gorillaone@gorillatactics.com>
2017-06-22 14:35:14 -0400

    Adding collision meshes to the diorama box.

---
+++ Assets/Scripts/PeriodicSoundEffect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PeriodicSoundEffect : MonoBehaviour {
+
+	public List<AudioClip> audioClips;
+	public int minSecondsToWait = 3;
+	public int maxSecondsToWait = 10; 
+
+	AudioSource source;
+	bool playingEffects = false; 
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playingEffects == false)
+		{
+			StartCoroutine(PlayRandomAudio()); 
+		}
+	}
+
+	private IEnumerator PlayRandomAudio()
+	{
+		playingEffects = true;
+		System.Random randomClassInstance = new System.Random(); //This instantiates a new instance of the Random object so we can get random numbers. 
+		int randomSecondsToWait = randomClassInstance.Next(minSecondsToWait, maxSecondsToWait);
+
+		yield return new WaitForSeconds(randomSecondsToWait);
+
+		int numberOfSongs = audioClips.Count; //Gets the number of songs we have in the list. 
+		int songIndex = randomClassInstance.Next(numberOfSongs); //This returns in integer between 0 and the maximum number of songs we have. 
+																					//This gets the song using that index and plays it. 
+		source.clip = audioClips[songIndex];
+		source.Play();
+
+		yield return new WaitForEndOfFrame();
+		yield return new WaitUntil(() => source.isPlaying == false); 
+
+		playingEffects = false;
+	}
+}
